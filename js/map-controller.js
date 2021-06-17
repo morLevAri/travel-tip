6f02d0d69c73b0940c8181c867e6a6b894a5d358
Mor Lev Ari <morshva@gmail.com>
2021-06-17 14:38:42 +0300

    first commit

---
+++ js/map-controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import { mapService } from './services/map-service.js'
+
+console.log('Controller is ready!');
+
+mapService.start()
---
+++ js/services/map-service.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// import { utilService } from './util-service.js'
+
+export const mapService = {
+    start
+}
+
+function start() {
+    console.log('start project');
+    // utilService.makeId()
+}
+
+
