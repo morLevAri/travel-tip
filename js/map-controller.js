38a9022f4985f4c01bad08f9d2265c5d8f4cb683
Mor Lev Ari <morshva@gmail.com>
2021-06-18 22:38:16 +0300

    added google map api

--- js/map-controller.js
+++ js/map-controller.js
@@ -2,6 +2,41 @@
 
 import { mapService } from './services/map-service.js'
 
+var gMap;
+
 console.log('Controller is ready!');
 
 mapService.start()
+
+window.onload = () => {
+    initMap()
+}
+
+export function initMap(lat = 32.0749831, lng = 34.9120554) {
+    console.log('InitMap');
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available');
+            gMap = new google.maps.Map(
+                document.querySelector('.map'), {
+                center: { lat, lng },
+                zoom: 15
+            })
+            console.log('Map!', gMap);
+        })
+}
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    // const API_KEY = ''
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${mykey}`;
+    elGoogleApi.async = true;
+    document.body.append(elGoogleApi);
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
+
